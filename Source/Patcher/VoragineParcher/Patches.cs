0b2ee02b246abbc6f6c2a719c7e41d0c134c8cd5
Carlos Cp <carlosx_1302@hotmail.com>
2011-09-13 22:50:07 -0500

    Update Source Parcher 4.2.2.14545

--- Source/Patcher/VoragineParcher/Patches.cs
+++ Source/Patcher/VoragineParcher/Patches.cs
@@ -28,6 +28,22 @@ namespace VoragineParcher
             Init();
         }
     }
+	// WoW 4.2.2
+    class Patch422Win : PatchInfo
+    {
+        protected override void Init()
+        {
+            ExeLength = 10218136;
+
+            Patches.Add(new PatchBytes(0x8702A,
+                                       new byte[] { 0x8B, 0x55, 0x0C, 0x83, 0xFA, 0x02, 0x0F, 0x85, 0xB0, 0x00, 0x00, 0x00 },
+                                       new byte[] { 0xB8, 0x00, 0x00, 0x00, 0x00, 0x90, 0xE9, 0xA9, 0x00, 0x00, 0x00, 0x90 }));
+
+            Patches.Add(new PatchBytes(0x86655,
+                                       new byte[] { 0x74, 0x28 },
+                                       new byte[] { 0xEB, 0x28 }));
+        }
+    }
     // WoW 4.2.0a
     class Patch420aWin : PatchInfo
     {
@@ -39,7 +55,7 @@ namespace VoragineParcher
                                        new byte[] { 0x8B, 0x45, 0x0C, 0x83, 0xF8, 0x02, 0x0F, 0x85, 0xA8, 0x00, 0x00, 0x00 },
                                        new byte[] { 0xB8, 0x00, 0x00, 0x00, 0x00, 0x90, 0xE9, 0xA9, 0x00, 0x00, 0x00, 0x90 }));
 
-            Patches.Add(new PatchBytes(0x86525,
+            Patches.Add(new PatchBytes(0x86685,
                                        new byte[] { 0x74, 0x28 },
                                        new byte[] { 0xEB, 0x28 }));
         }
